0569abfcf8f3fb2c4c18885c606c85dfbd0764da
Yehor Boichenko <boichenko.yehor@gmail.com>
2023-03-15 18:46:45 +0200

    styles fix

--- rick-and-morty-app/src/pages/NotFound.tsx
+++ rick-and-morty-app/src/pages/NotFound.tsx
@@ -6,9 +6,9 @@ const NotFound = () => {
       <div className="space"></div>
       <div className="wrapper">
         <div className="img-wrapper">
-          <span>44</span>
+          <span className='notfound'>44</span>
         </div>
-        <p>
+        <p className='notfound-text'>
           The page you are trying to search has been <br /> moved to another
           universe.
         </p>
